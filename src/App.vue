1d43186d470bb690d87facf252216c2127bbb425
twzjh <night860610@gmail.com>
2024-03-03 21:10:05 +0800

    change card

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 <template>
   <header class="bg-black text-white">
     <div class="wrapper">
-      <nav class="h-[40px] flex items-center justify-between">
+      <nav class="h-[40px] flex items-center justify-between px-1">
         <RouterLink to="/shooping" class="text-yellow-500">shooping</RouterLink>
         <RouterLink to="/ShoppingCart">
         <span class="material-symbols-outlined mr-5 flex items-center">shopping_cart</span>
--- src/views/shooping.vue
+++ src/views/shooping.vue
@@ -3,6 +3,7 @@ export default{
   data(){
     return{
       products:[],
+      productsList: 'card',
     }
   },
   mounted(){
@@ -22,9 +23,18 @@ export default{
   <main class="main">
     <div>
       <h1 class="mb-3 text-3xl font-bold">商品列表</h1>
-      <div class="flex justify-end"><button>卡片顯示</button>/<button>條列顯示</button></div>
+      <div class="flex justify-end">
+        <button @click="productsList='card'">
+        <span class="material-symbols-outlined">dataset</span>
+        </button>
+        /
+        <button @click="productsList='list'">
+        <span class="material-symbols-outlined">list</span>
+        </button>
+      </div>
     </div>
-    <div class="flex flex-wrap justify-center gap-5">
+    <!-- 卡片顯示 -->
+    <div class="flex flex-wrap justify-center gap-5" v-if= "productsList == 'card'">
       <div v-for="(products, index) in products" :key="index" class="w-1/6 border rounded-lg flex flex-col">
         <img src="https://dummyimage.com/300x300" class="rounded-t-lg" alt="">
         <div class="pl-3 pt-3 gap-3 flex flex-col h-[200px]">
@@ -36,6 +46,35 @@ export default{
         <button class="bg-yellow-800 rounded-b-lg px-4 py-1">加入購物車</button>
       </div>
     </div>
+    
+    <!-- 條列顯示 -->
+    <div class="mb-6 px-5" v-else-if = "productsList == 'list'">
+      <div class="grid grid-cols-6 gap-4 border-y py-2 font-bold">
+        <div class="text-center">商品圖片</div>
+        <div>商品名稱</div>
+        <div class="text-center">商品描述</div>
+        <div class="text-center">價錢</div>
+        <div></div>
+        <div></div>
+      </div>
+      <div v-for="(products, index) in products" :key="index" class="grid grid-cols-6 gap-x-3 py-3 border-b">
+        <div class="flex justify-center"><img src="https://dummyimage.com/300x300"></div>
+        <div class="flex items-center">{{ products.name }}</div>
+        <div class="flex items-center justify-center">{{ products.description }}</div>
+        <div class="flex items-center justify-center">${{ products.price }}</div>
+        <div class="flex items-center justify-center">
+          <div class="flex flex-col">
+            <div>
+              <button class="button-left">-</button>
+              <input type="number" class="border w-[80px]">
+              <button class="button-right">+</button>
+            </div>
+            <button class="bg-yellow-800 rounded-full px-4 py-1">加入購物車</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </main>
 </template>
 
